8b2d29a2d8297f0a21b5d3ac1845b8ecb6622a23
Gustavo Ribeiro <gustavo_ribeiro@trimble.com>
2020-05-18 20:06:11 -0300

    Subindo Aula 2

---
+++ Program2.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace calculadoraApp2
+{
+    //aula 2
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("testando");
+        }
+    }
+}
