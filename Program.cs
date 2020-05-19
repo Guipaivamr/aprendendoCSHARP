1bdb9ffcb7786f658d9352c6c84df60627ec1f56
Guilherme Paiva <gui.paivamr@hotmail.com>
2020-05-18 23:12:43 -0300

    Iniciando Projeto

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace calculadoraApp
 {
@@ -6,7 +7,39 @@ namespace calculadoraApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("NOME: ");
+            string nome = Console.ReadLine();
+            Console.Write("CIDADE: ");
+            string cidade = Console.ReadLine();
+            Console.Write("ENDEREÇO: ");
+            string endereço = Console.ReadLine();
+            Console.Write("NUMERO: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("TELEFONE: ");
+            string telefone = Console.ReadLine();
+           
+            int x, y;
+            
+
+            Console.WriteLine("Entrada de saldo");
+            x = int.Parse(Console.ReadLine());
+            y = x;
+            Console.WriteLine("Seu saldo acumulado é " + x);
+
+            while ( y != 0) {               
+                Console.WriteLine("Entrada de saldo: ");  
+                y = int.Parse(Console.ReadLine());
+                x = x + y; 
+                Console.WriteLine("Seu saldo acumulado é " + x);
+            }
+            Console.WriteLine("Entrada de saldo : 0");
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Cidade: " + cidade);
+            Console.WriteLine("Endereço: " + endereço + " Numero " + numero);
+            Console.WriteLine("Telefone: " + telefone);
+
+            Console.WriteLine("Saldo atual: " + x);
+
         }
     }
 }
